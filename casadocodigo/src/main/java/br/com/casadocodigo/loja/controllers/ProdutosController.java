a2b93eac21463392e2ecb3361536b7747f4c0a49
Tauã Brandão <brandao.taua@gmail.com>
2020-02-29 12:52:07 -0300

    Validação com Validator do Spring

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -2,8 +2,13 @@ package br.com.casadocodigo.loja.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,12 +18,18 @@ import br.com.casadocodigo.loja.dao.ProdutoDAO;
 import br.com.casadocodigo.loja.models.Produto;
 import br.com.casadocodigo.loja.models.TipoPreco;
 import br.com.casadocodigo.loja.util.ReturnView;
+import br.com.casadocodigo.loja.validation.ProdutoValidation;
 
 @Controller
 @RequestMapping("/produtos")
 public class ProdutosController {
 
 	private final String viewFolder = "produtos";
+
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.addValidators(new ProdutoValidation());
+	}
 
 	@Autowired
 	private ProdutoDAO dao;
@@ -34,7 +45,12 @@ public class ProdutosController {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView gravar(Produto produto, RedirectAttributes redirectAttributes) {
+	public ModelAndView gravar(@Valid Produto produto, BindingResult result, RedirectAttributes redirectAttributes) {
+
+		if (result.hasErrors()) {
+			return this.form();
+		}
+
 		String retorno = "redirect:produtos";
 
 		System.out.println(produto);
